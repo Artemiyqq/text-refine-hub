45069b3024fb701663aef1c123580f53a2f62c82
ArtemFlorko <artom.florko@gmail.com>
2024-01-15 00:27:39 +0200

    Styling a component for inserting text or uploading DOCX

--- client/src/app/core/text-box-variant/text-box-variant.component.ts
+++ client/src/app/core/text-box-variant/text-box-variant.component.ts
@@ -1,17 +1,26 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-text-box-variant',
   templateUrl: './text-box-variant.component.html',
   styleUrls: ['./text-box-variant.component.scss']
 })
-export class TextBoxVariantComponent {
+export class TextBoxVariantComponent implements OnInit {
   @Input() variant: 'paste text' | 'see result' = 'paste text';
   @Input() passedText: string = '';
 
   @Output() getText = new EventEmitter<string>();
 
   text: string = '';
+
+  ngOnInit(): void {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl: any) {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  }
 
   handleProcessButtonClick(): void {
     this.getText.emit(this.text);
